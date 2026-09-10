f326db7b56a3d521acca9ab1ab2608f430f984f4
Manik <manikkori697@gmail.com>
2026-09-10 19:14:53 +0530

    added uploadMiddleware

---
+++ backend/src/middlewares/uploadMiddleware.js
@@ -0,0 +1,32 @@
+const multer = require("multer");
+const AppError = require("../utils/AppError");
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/webp"
+  ) {
+    cb(null, true);
+  } else {
+    cb(
+      new AppError(
+        "Not an image! Please upload only jpeg, png, or webp files.",
+        400,
+      ),
+      false,
+    );
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+  fileFilter: fileFilter,
+});
+
+module.exports = upload;
