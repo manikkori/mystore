e76708408cab328a16804cbedac57f1e93acc0f5
Manik <manikkori697@gmail.com>
2026-09-12 12:05:37 +0530

    Sort user order history by newest first

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -39,7 +39,7 @@ exports.createOrder = async (req, res, next) => {
 
 exports.getMyOrders = async (req, res, next) => {
   try {
-    const orders = await Order.find({ user: req.user._id });
+    const orders = await Order.find({ user: req.user._id }).sort({ createdAt: -1 });
     res.status(200).json({ success: true, orders });
   } catch (error) {
     next(error);
